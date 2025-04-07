7fb3884faa27e50e0ebe3bbcc5144f7353102840
matsvei-kavalenka <matsvei.kavalenka@gmail.com>
2025-04-07 17:44:46 +0300

    Finished first micro-service setup

--- src/main/java/lt/vilniustech/mkavalenka/investment/InvestmentApplication.java
+++ src/main/java/lt/vilniustech/mkavalenka/investment/InvestmentApplication.java
@@ -2,7 +2,9 @@ package lt.vilniustech.mkavalenka.investment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InvestmentApplication {
 
